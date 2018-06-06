97de6bd90b5ed348f66e184def3ccd056bd0a51f
Saurabh Agrawal <39870501+SaurabhAgrawal1010@users.noreply.github.com>
2018-06-06 06:52:59 +0530

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,7 +1,7 @@
 set head off
 set feed off
 set scan off
-select stage,';',customer_troublenumber,';',priority,';',created,';',issue_type,proj,';',summary,';',assignee,';',substatus,';',identified_in_version,';',required_for_version,';',stage,';@;'
-from jira
+select issue_number,';',issue_detail,';',priority,';',created,';',issue_type,';',summary,';',assignee,';',substatus,';@;'
+from table_name
 order by created desc;
 
